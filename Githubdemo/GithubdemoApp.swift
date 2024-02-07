2b48e55522298a5a72bbfbe9ca70e77b3804d4c1
Ethan Mathew <ebnethan3@gmail.com>
2024-02-06 22:36:11 -0800

    Initial Commit

---
+++ Githubdemo/GithubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubdemoApp.swift
+//  Githubdemo
+//
+//  Created by Admin on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
